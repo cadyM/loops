ccb014669990a87bef43c322c73b273f558f4408
Apple <apple@Apples-MacBook-Pro-46.local>
2019-07-09 15:10:41 -0500

    Initial Commit

---
+++ Contents.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+////Created an array to store Kode with Klossy Sponsors!
+//var sponsors = ["Addias" , "Estte Lauder", "Caroline Herra Good Girls", "Apple" , "Wework"]
+//
+////I am going to write a loop that prints all the sponsors
+//for sponsor in sponsors{
+//    print("Shout out to \(sponsor) for helping KWK Happen!! 😀")
+//}
+//
+//var capitals = ["France" : "Paris" , "Cuba" : "Havana" , "Japan" : "Tokyo"]
+////for pair in capitals  {
+////print(pair)
+////}
+//
+////for (country, capital) in capitals{
+////    print( "\(capital) is the capital of \(country)")
+////}
+//
+//for pair in capitals{
+//    print (pair.key)
+//}
+//
+
+
+
+//var map = ["Sacremento Ca." : "577" , "Clackamas Or." : "25" , "Salem Or. " : "44"]
+//for (city ,  distance) in  map{
+//    print( "You are currently \(distance) Miles away from \(city)!")
+//}
+
+//for _ in 1...4 {
+//    print("hello!")
+//}
+//
+////Print Shoutout to KWK Scholars 20 times
+//for _ in 1...20{
+//    print("Shoutout to Portland KWK Scholars")
+//}
+
+var arrayOne : [String] = []
+var arrayTwo : [String] = []
+
+var dictonary : [String: String] = [:]
+
+for (index, element) in arrayOne.enumerated(){
+    dictonary[ element = arrayOne[index]]
